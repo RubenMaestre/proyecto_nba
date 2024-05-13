2649ddb2491594e6d2e693d2b3663b0491c7e1fc
RubenMaestre <formacion@rubenmaestre.com>
2024-05-13 16:14:32 +0200

    actualiza

--- streamlit/paginas/limpieza.py
+++ streamlit/paginas/limpieza.py
@@ -156,5 +156,168 @@ def display():
     - Guardamos el DataFrame resultante en un archivo Excel, asegurando que todos los datos estén actualizados y bien organizados para su uso en análisis posteriores.
     """)
 
+
+    # Título centrado para la sección de conexión con API Airtable
+    st.markdown("<h2 style='text-align: center;'>CONEXIÓN CON API AIRTABLE</h2><br>", unsafe_allow_html=True)
+
+    # Introducción y configuración inicial para la conexión con Airtable
+    st.header("Configuración Inicial y Envío de Datos a Airtable")
+    st.markdown("""
+        **Configuración Inicial:**
+        - **API Key**: Clave de usuario para autenticarse con la API de Airtable.
+        - **Base ID y Table ID**: Identificadores únicos para la base de datos y la tabla dentro de Airtable donde se enviarán los datos.
+        - **Headers**: Configuración necesaria para las solicitudes HTTP, incluyendo la autorización y el tipo de contenido.
+        """)
+
+    # Código para la configuración inicial de la conexión con Airtable
+    st.code("""
+    API_KEY = "xxxxx"  # Usuario
+    BASE_ID = "xxxxxx"  # Base: Ensaladas - API
+    TABLE_ID = "xxxx"  # Tabla: datos_1
+
+    airtable_base_url = "https://api.airtable.com/v0"
+
+    # Headers para la autenticación y especificación del contenido
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    print("Headers:")
+    pprint(headers)
+
+    # Endpoint para la tabla específica en Airtable
+    endpoint = f"{airtable_base_url}/{BASE_ID}/{TABLE_ID}"
+    print("Endpoint:")
+    print(endpoint)
+    """, language='python')
+
+    st.markdown("""
+        **Envío de Datos:**
+        - Se preparan los datos a enviar en grupos de registros utilizando pandas y se envían a Airtable en lotes de 10 registros para gestionar mejor el rendimiento y los límites de la API.
+        - Se utiliza un bucle para enviar todos los registros presentes en el DataFrame, asegurando que todos los datos se transfieran correctamente.
+        """)
+
+    # Código para el envío de datos a Airtable
+    st.code("""
+    # Cargar los datos desde un archivo Excel actualizado
+    archivo_excel = 'excels/actualizados/equipos_nba_actualizado.xlsx'
+    df1 = pd.read_excel(archivo_excel)
+    df1 = df1.replace({np.nan: None})  # Reemplazar NaN por None para compatibilidad con JSON
+
+    # Preparar datos para enviar a Airtable
+    datos_subir = {
+        "records": [{"fields": df1.iloc[i, :].to_dict()} for i in range(min(10, len(df1)))],
+        "typecast": True
+    }
+
+    print("Datos a subir:")
+    pprint(datos_subir)
+
+    # Función para enviar datos a Airtable
+    def enviar_a_airtable(grupo_datos):
+        response = requests.post(url=endpoint, json=grupo_datos, headers=headers)
+        if response.status_code == 200:
+            print("Datos enviados correctamente")
+        else:
+            print("Error al enviar datos:", response.status_code)
+
+    # Bucle para enviar los datos en grupos de 10
+    for start in range(0, len(df1), 10):
+        end = min(start + 10, len(df1))  # No sobrepasar el tamaño del DataFrame
+        grupo_datos = {
+            "records": [{"fields": row.to_dict()} for _, row in df1.iloc[start:end].iterrows()],
+            "typecast": True
+        }
+        enviar_a_airtable(grupo_datos)
+    """, language='python')
+
+    st.markdown("""
+        Esta sección detalla el proceso de conectar con Airtable y enviar datos estructurados directamente desde Python, facilitando la integración y automatización de flujos de trabajo de datos.
+        """)
+    
+
+    # Subtítulo para la nueva sección de envío de datos de jugadores
+    st.markdown("<h3 style='text-align: center;'>Envío de la tabla de estadísticas de jugadores a Airtable</h3><br>", unsafe_allow_html=True)
+
+    # Explicación del proceso de configuración para una nueva tabla en Airtable
+    st.header("Configuración y Envío de Estadísticas de Jugadores")
+    st.markdown("""
+        A continuación, configuro el entorno para enviar datos de estadísticas de jugadores a una tabla diferente en Airtable. Esto implica configurar nuevas credenciales y preparar los datos específicamente para esta tabla.
+        
+        **Configuración de Credenciales:**
+        - **API Key, Base ID, Table ID**: Se actualizan para acceder a la tabla específica de estadísticas de jugadores en Airtable.
+        - **Headers y Endpoint**: Se configuran para autenticar y dirigir las solicitudes a la API adecuada.
+        """)
+
+    # Código para configuración y envío de datos de estadísticas de jugadores
+    st.code("""
+    API_KEY = "xxxxxxx"  # Usuario
+    BASE_ID = "xxxxxxx"  # Base
+    TABLE_ID = "xxxxxx"  # Tabla de estadísticas
+
+    airtable_base_url = "https://api.airtable.com/v0"
+
+    # Configuración de headers para la autenticación
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    print("Headers configurados:")
+    pprint(headers)
+
+    # Construcción del endpoint específico para la tabla
+    endpoint = f"{airtable_base_url}/{BASE_ID}/{TABLE_ID}"
+    print("Endpoint para estadísticas de jugadores:")
+    print(endpoint)
+
+    # Preparación de parámetros para la solicitud
+    params = {
+        "fields": None, 
+        "maxRecords": None, 
+        "pageSize": None,
+        "returnFieldsByFieldId": None
+    }
+
+    print("Parámetros configurados:")
+    pprint(params, sort_dicts=False)
+
+    # Cargar los datos de jugadores desde un archivo Excel
+    archivo_excel = 'excels/actualizados/jugadores_completos.xlsx'
+    df2 = pd.read_excel(archivo_excel)
+    df2 = df2.replace({np.nan: None})  # Preparar los datos para ser compatibles con JSON
+
+    # Preparar los datos a enviar en forma de registros
+    datos_subir_jug = {
+        "records": [{"fields": df2.iloc[i, :].to_dict()} for i in range(min(10, len(df2)))],
+        "typecast": True
+    }
+
+    print("Datos preparados para enviar:")
+    pprint(datos_subir_jug)
+
+    # Función para enviar datos a Airtable
+    def enviar_a_airtable(grupo_datos):
+        response = requests.post(url=endpoint, json=grupo_datos, headers=headers)
+        if response.status_code == 200:
+            print("Datos enviados correctamente")
+        else:
+            print("Error al enviar datos:", response.status_code)
+
+    # Bucle para enviar los datos en grupos de 10
+    for start in range(0, len(df2), 10):
+        end = min(start + 10, len(df2))
+        grupo_datos = {
+            "records": [{"fields": row.to_dict()} for _, row in df2.iloc[start:end].iterrows()],
+            "typecast": True
+        }
+        enviar_a_airtable(grupo_datos)
+    """, language='python')
+
+    st.markdown("""
+        Este proceso garantiza que cada conjunto de datos se envíe correctamente y maneja la paginación de la API de Airtable para evitar sobrecargar la solicitud con demasiados datos de una vez. Así, aseguramos una transferencia eficiente y ordenada de los datos hacia la base de Airtable.
+        """)
+
 # Llama a la función para mostrar la página
 display()
