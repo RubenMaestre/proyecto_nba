6af6fcec9114612f0b531a425e9edeb06b39d2af
RubenMaestre <formacion@rubenmaestre.com>
2024-05-13 15:46:25 +0200

    Update limpieza.py

--- streamlit/paginas/limpieza.py
+++ streamlit/paginas/limpieza.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def display():
-    st.title('Extracción y análisis de datos de la NBA')
+    st.title('Limpieza de datos y envío a Airtable')
 
 # Llama a la función para mostrar la página
 display()
