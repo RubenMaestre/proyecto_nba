67ca33de7c4c22efaf7c1ed823122e175821be98
RubenMaestre <formacion@rubenmaestre.com>
2024-05-13 20:18:32 +0200

    Update grafica_3.py

--- streamlit/modules/graph/grafica_3.py
+++ streamlit/modules/graph/grafica_3.py
@@ -3,37 +3,29 @@ import pandas as pd
 import folium
 import streamlit as st
 from folium.features import CustomIcon
+from streamlit_folium import st_folium
 
 def mapa_equipos_nba():
-    # Cargar datos
     ruta_equipos_nba = 'excels/actualizados/datos_nuevos_equipos_nba.xlsx'
     df_equipos_nba = pd.read_excel(ruta_equipos_nba)
 
-    # Crear un mapa base
     mapa = folium.Map(location=[37.0902, -95.7129], zoom_start=4)
 
-    # Agregar marcadores al mapa
     for _, fila in df_equipos_nba.iterrows():
         latitud = fila['Latitud']
         longitud = fila['Longitud']
         id_equipo = fila['ID Equipo']
         nombre_equipo = fila['Nombre Equipo']
         ruta_logo = f"logos_equipos/png/{id_equipo}.png"
-
-        icono = CustomIcon(
-            icon_image=ruta_logo,
-            icon_size=(30, 30)
-        )
-
+        
+        icono = CustomIcon(icon_image=ruta_logo, icon_size=(30, 30))
+        
         folium.Marker(
             [latitud, longitud],
             popup=nombre_equipo,
             icon=icono
         ).add_to(mapa)
+    
+    # Utiliza st_folium para mostrar el mapa en Streamlit
+    st_folium(mapa, width=725, height=500)
 
-    # Mostrar el mapa en Streamlit
-    st_folium = st.components.v1.components.declare_component(
-        "streamlit_folium", path="frontend"
-    )
-
-    st_folium(map_data=mapa._repr_html_(), width=960, height=600)
