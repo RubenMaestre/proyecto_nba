edccc563eb9af8b92d53da5e042179f6a029f4bf
RubenMaestre <formacion@rubenmaestre.com>
2024-05-14 09:18:02 +0200

    Update grafica_4.py

--- streamlit/modules/graph/grafica_4.py
+++ streamlit/modules/graph/grafica_4.py
@@ -18,6 +18,7 @@ def grafica_top_jugadores():
     fig, axs = plt.subplots(5, 2, figsize=(15, 20))  # Ajustar tamaño según necesidades
     fig.suptitle('Top 10 jugadores en varias categorías')
 
+    # Dibuja las primeras 9 gráficas en las primeras posiciones
     for i, cat in enumerate(categorias):
         ax = axs[i % 5, i // 5]  # Ubicar cada subplot en la posición correcta
         top_jugadores = df_jugadores_nba.nlargest(10, cat)
@@ -25,6 +26,10 @@ def grafica_top_jugadores():
         ax.set_title(f'Top 10 en {cat}')
         ax.set_xlabel('')
         ax.set_ylabel('')
+
+    # Opción para dejar en blanco el último gráfico
+    for j in range(i + 1, 10):
+        axs[j % 5, j // 5].axis('off')
 
     plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Ajustar el layout para el título superior
     st.pyplot(fig)
