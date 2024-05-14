7e58391396a0bd59657423972642a4abaf44bc01
RubenMaestre <formacion@rubenmaestre.com>
2024-05-14 09:10:13 +0200

    Update grafica_4.py

--- streamlit/modules/graph/grafica_4.py
+++ streamlit/modules/graph/grafica_4.py
@@ -15,18 +15,17 @@ def grafica_top_jugadores():
     categorias = ['PTS', '3PM', 'FTM', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK']
 
     # Crear una figura y varios subplots
-    fig, axs = plt.subplots(3, 3, figsize=(15, 10))  # Ajustar el tamaño según necesidad
+    fig, axs = plt.subplots(5, 2, figsize=(15, 20))  # Ajusta aquí las dimensiones según necesites
     fig.suptitle('Top 10 jugadores en varias categorías')
 
+    # Iterar sobre cada categoría y asignar a un subplot
     for i, cat in enumerate(categorias):
-        ax = axs[i // 3, i % 3]  # Ubicar cada subplot en la posición correcta
+        ax = axs[i % 5, i // 5]  # Ubicar cada subplot en la posición correcta
         top_jugadores = df_jugadores_nba.nlargest(10, cat)
         sns.barplot(x=cat, y='Nombre Completo', data=top_jugadores, ax=ax)
         ax.set_title(f'Top 10 en {cat}')
         ax.set_xlabel('')
         ax.set_ylabel('')
 
-    plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # Ajustar el layout para el título superior
-    st.pyplot(fig)  # Mostrar la figura en Streamlit
-
-# Esta función podría ser llamada desde la página de EDA
+    # Ajustar el layout para evitar superposición de títulos y labels
+    plt.tight_layou
