eaf4e378f008a8dd7913f0ae5f5515ae87ee5332
RubenMaestre <formacion@rubenmaestre.com>
2024-05-14 16:03:06 +0200

    Update fichas_nba.py

--- streamlit/paginas/fichas_nba.py
+++ streamlit/paginas/fichas_nba.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 from modules.calculos_finales import calcular_puntuaciones
-from modules.tarjetas_nba import crear_ficha_jugador
+#from modules.tarjetas_nba import crear_ficha_jugador
 
 def display():
     st.markdown("<h1 style='text-align: center;'>Fichas de jugadores de la NBA</h1>", unsafe_allow_html=True)
