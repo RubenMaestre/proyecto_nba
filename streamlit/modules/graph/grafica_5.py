65f6b6764abed1319aa2cb104dda234390483dbc
RubenMaestre <formacion@rubenmaestre.com>
2024-05-14 10:03:09 +0200

    Update grafica_5.py

--- streamlit/modules/graph/grafica_5.py
+++ streamlit/modules/graph/grafica_5.py
@@ -1,7 +1,6 @@
 # modules/graph/grafica_5.py
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
 import streamlit as st
 
 def grafica_correlacion_ppg_apg():
@@ -10,6 +9,7 @@ def grafica_correlacion_ppg_apg():
     Esta gráfica de dispersión explora la relación entre los puntos anotados por partido y las asistencias por partido de los jugadores de la NBA durante la temporada 2023/2024. 
     Los puntos por partido (PPG) representan la capacidad ofensiva de un jugador, mientras que las asistencias por partido (APG) reflejan su habilidad para facilitar el juego y contribuir al éxito del equipo de manera colaborativa.
     """, unsafe_allow_html=True)
+
     # Carga de datos
     ruta_jugadores_nba = 'excels/actualizados/jugadores_completos.xlsx'
     df_jugadores_nba = pd.read_excel(ruta_jugadores_nba)
@@ -21,12 +21,17 @@ def grafica_correlacion_ppg_apg():
     df_para_graficar.dropna(subset=['PPG', 'APG'], inplace=True)
     df_para_graficar = df_para_graficar[(df_para_graficar['PPG'] > 0) & (df_para_graficar['APG'] > 0)]
 
-    # Creación de la gráfica de dispersión
-    plt.figure(figsize=(10, 8))
-    sns.scatterplot(x='PPG', y='APG', data=df_para_graficar, color='blue')
+    # Creación de la gráfica de dispersión interactiva con Plotly
+    fig = px.scatter(df_para_graficar, x='PPG', y='APG',
+                     title='Correlación entre puntos por partido (PPG) y asistencias por partido (APG)',
+                     labels={'PPG': 'Puntos por partido (PPG)', 'APG': 'Asistencias por partido (APG)'},
+                     hover_data=['Nombre del Jugador'])
 
-    plt.title('Correlación entre puntos por partido (PPG) y asistencias por partido (APG)')
-    plt.xlabel('Puntos por partido (PPG)')
-    plt.ylabel('Asistencias por partido (APG)')
+    fig.update_traces(marker=dict(color='blue', size=10),
+                      selector=dict(mode='markers'))
 
-    st.pyplot(plt.gcf()) 
+    fig.update_layout(title_font_size=24,
+                      xaxis_title_font_size=18,
+                      yaxis_title_font_size=18)
+
+    st.plotly_chart(fig)
