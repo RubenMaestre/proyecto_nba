6d66a856d26b0c3e5671d0afddddfc1452247ed0
RubenMaestre <formacion@rubenmaestre.com>
2024-05-14 15:52:06 +0200

    Update calculos_finales.py

--- streamlit/modules/calculos_finales.py
+++ streamlit/modules/calculos_finales.py
@@ -1,47 +1,29 @@
 import pandas as pd
 
-def cargar_datos(ruta_equipos, ruta_jugadores):
-    """
-    Carga los datos de los equipos y jugadores desde archivos Excel.
+def calcular_puntuaciones():
+    # Lo primero es importar los datos descargados de airtable que tenemos almacenados en un excel para hacer gráficas. 
+    ruta_equipos_nba = 'excels/actualizados/datos_nuevos_equipos_nba.xlsx'
+    ruta_jugadores_nba = 'excels/actualizados/jugadores_completos.xlsx'
 
-    Args:
-    ruta_equipos (str): Ruta del archivo Excel con los datos de los equipos.
-    ruta_jugadores (str): Ruta del archivo Excel con los datos de los jugadores.
+    df_equipos_nba = pd.read_excel(ruta_equipos_nba)
+    df_jugadores_nba = pd.read_excel(ruta_jugadores_nba)
 
-    Returns:
-    pd.DataFrame: DataFrame con los datos de los jugadores.
-    """
-    df_equipos_nba = pd.read_excel(ruta_equipos)
-    df_jugadores_nba = pd.read_excel(ruta_jugadores)
-    return df_jugadores_nba
+    # Vamos a calcular de todos los jugadores respecto a los partidos jugados la media, varianza y cuartiles 1 y 3 de todos sus datos
 
-def calcular_puntuaciones(df_jugadores_nba):
-    """
-    Calcula las puntuaciones de los jugadores y devuelve el DataFrame con las puntuaciones finales.
-
-    Args:
-    df_jugadores_nba (pd.DataFrame): DataFrame que contiene los datos de los jugadores.
-
-    Returns:
-    pd.DataFrame: DataFrame con las puntuaciones finales de los jugadores.
-    """
-    print("Inicio de la función calcular_puntuaciones")
-    
-    estadisticas = ['MIN', 'PTS', 'FGM', 'FGA', '3PM', '3PA', 'FTM', 'FTA', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'EFF']
-    
-    # Crear un DataFrame para trabajar
+    # Lo primero vamos a crear un nuevo dataframe que sea copia del original para trabajar en él y tener el original por si necesitamos incorporar algún dato sin modificar
     df_estadisticas_jugadores = df_jugadores_nba.copy()
+    
+    # Calculamos la media
+    estadisticas = ['MIN', 'PTS', 'FGM', 'FGA', '3PM', '3PA', 'FTM', 'FTA', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'EFF']
     for stat in estadisticas:
         df_estadisticas_jugadores[stat + '_por_GP'] = df_estadisticas_jugadores[stat] / df_estadisticas_jugadores['GP']
-    
-    # Filtrar jugadores que han jugado al menos un partido
-    df_estadisticas_jugadores = df_estadisticas_jugadores[df_estadisticas_jugadores['GP'] != 0]
-    print("Filtrado jugadores que han jugado al menos un partido")
 
-    # Calcular varianzas y cuartiles
+    # Vamos a quitar los datos de los jugadores que no hayan jugado ningún partido para no dividir entre 0
+    df_estadisticas_jugadores = df_estadisticas_jugadores[df_estadisticas_jugadores['GP'] != 0]
+
+    # Y hacemos lo mismo con la varianza y cuartiles
     varianzas = df_estadisticas_jugadores[estadisticas].var()
     cuartiles = df_estadisticas_jugadores[estadisticas].quantile([0.25, 0.75])
-    print("Calculadas varianzas y cuartiles")
 
     # Paso 2: Normalizar las estadísticas
     def normalizar_min_max(df, columnas):
@@ -53,7 +35,6 @@ def calcular_puntuaciones(df_jugadores_nba):
 
     columnas_normalizadas = [stat + '_por_GP' for stat in estadisticas]
     df_estadisticas_normalizadas = normalizar_min_max(df_estadisticas_jugadores, columnas_normalizadas)
-    print("Estadísticas normalizadas")
 
     # Paso 3: Ajustar puntuaciones según los cuartiles
     def ajustar_por_cuartiles(df, columnas, q1, q3, incremento_q3, decremento_q1):
@@ -67,7 +48,6 @@ def calcular_puntuaciones(df_jugadores_nba):
     incremento_q3 = 1
     decremento_q1 = 1
     df_puntuaciones_ajustadas = ajustar_por_cuartiles(df_estadisticas_normalizadas, [col + '_normalizado' for col in columnas_normalizadas], cuartiles.loc[0.25], cuartiles.loc[0.75], incremento_q3, decremento_q1)
-    print("Ajustadas puntuaciones según cuartiles")
 
     # Paso 4: Ajustar puntuaciones según la varianza
     def ajustar_por_varianza(df, columnas, varianzas, umbral_varianza, ajuste_varianza):
@@ -84,22 +64,17 @@ def calcular_puntuaciones(df_jugadores_nba):
     umbral_varianza = 1.5
     ajuste_varianza = 0.5
     df_puntuaciones_finales = ajustar_por_varianza(df_puntuaciones_ajustadas, [col + '_normalizado' for col in columnas_normalizadas], varianzas, umbral_varianza, ajuste_varianza)
-    print("Ajustadas puntuaciones según varianza")
 
-    # Calcular la media de puntos obtenidos
+    # Ahora vamos a calcular la media de puntos que han obtenido y ver cuál es el mejor jugador según nuestros cálculos de la NBA
     columnas_puntuaciones = [stat + '_por_GP_normalizado' for stat in estadisticas]
     df_puntuaciones_finales['Puntuacion_Total'] = df_puntuaciones_finales[columnas_puntuaciones].mean(axis=1)
 
-    # Expresar el resultado en formato tarjeta FIFA (valoración de 1 a 100)
+    # Y quiero expresar el resultado en formato tarjeta FIFA que es valoración de 1 a 100
     df_puntuaciones_finales['Puntuacion_Total'] = (df_puntuaciones_finales['Puntuacion_Total'] * 10).round(2)
-    print("Calculada Puntuacion_Total")
 
     return df_puntuaciones_finales
 
 # Llama a la función para obtener los resultados finales
 if __name__ == "__main__":
-    ruta_equipos_nba = 'excels/actualizados/datos_nuevos_equipos_nba.xlsx'
-    ruta_jugadores_nba = 'excels/actualizados/jugadores_completos.xlsx'
-    df_jugadores_nba = cargar_datos(ruta_equipos_nba, ruta_jugadores_nba)
-    df_resultados = calcular_puntuaciones(df_jugadores_nba)
+    df_resultados = calcular_puntuaciones()
     print(df_resultados[['Nombre', 'Apellido', 'Puntuacion_Total']].sort_values(by='Puntuacion_Total', ascending=False))
